2d21daae9d9203cfa9363c9e5423ae0663fb28e3
GerhardVFX <90000584+GerryVFX@users.noreply.github.com>
2023-03-23 16:39:48 -0600

    ShuterPlayerInstanciate

--- ProjectFiestaTest/Assets/_Scripts/MiniGames/Shooter/Sh_PlayerManager.cs
+++ ProjectFiestaTest/Assets/_Scripts/MiniGames/Shooter/Sh_PlayerManager.cs
@@ -1,18 +1,48 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Sh_PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PhotonView pv;
+
+    private void Awake()
     {
-        
+        pv = GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        if (pv.IsMine) CreateControll();
+    }
+
+    void CreateControll()
+    {
+        switch (PlayerSettings.Instance.characterID)
+        {
+            case 0:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player00"), Vector3.zero, Quaternion.identity);
+                break;
+            case 1:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player01"), Vector3.zero, Quaternion.identity);
+                break;
+            case 2:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player02"), Vector3.zero, Quaternion.identity);
+                break;
+            case 3:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player03"), Vector3.zero, Quaternion.identity);
+                break;
+            case 4:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player04"), Vector3.zero, Quaternion.identity);
+                break;
+            case 5:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player05"), Vector3.zero, Quaternion.identity);
+                break;
+            case 6:
+                PhotonNetwork.Instantiate(Path.Combine("ShooterPB", "Player06"), Vector3.zero, Quaternion.identity);
+                break;
+        }
     }
 }
